c60987ab88fcabd1476724caf1b9549368eaf0c9
lVeliarl <dimalitx@gmail.com>
2018-09-30 06:36:21 +0300

    zeros/imp: implemented getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let count = 0;
+  for (let i = 5; number / i >= 1; i *= 5) {
+     count = count + Math.floor(number / i);
+  }
+  return count;
 }
